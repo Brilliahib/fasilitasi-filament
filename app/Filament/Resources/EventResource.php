dc898d5ad3426ab209e089cca7c7acc9ae3d53e5
brilliahib <brilliahib21@gmail.com>
2024-07-18 13:25:03 +0700

    add: card on dashboard

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\EventResource\Pages;
 use App\Filament\Resources\EventResource\RelationManagers;
 use App\Models\Event;
 use Filament\Forms;
+use Filament\Forms\Components\Card;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
@@ -16,6 +17,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -28,25 +30,28 @@ class EventResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
+        return $form->schema([
+            Card::make()->schema([
                 TextInput::make('title')->required()->maxLength(255),
                 ColorPicker::make('color'),
                 DateTimePicker::make('start_at'),
                 DateTimePicker::make('end_at'),
-                Select::make('keterangan')
-                ->options([
-            'remote' => 'Remote',
-        'on site' => 'On Site',
+                Select::make('keterangan')->options([
+                    'remote' => 'Remote',
+                    'on site' => 'On Site',
                 ]),
-                FileUpload::make('image')
-            ]);
+                FileUpload::make('image'),
+            ]),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
+                TextColumn::make('No')->state(static function (HasTable $livewire, $rowLoop): string {
+                    return (string) ($rowLoop->iteration + $livewire->getTableRecordsPerPage() * ($livewire->getTablePage() - 1));
+                }),
                 TextColumn::make('title'),
                 ColorColumn::make('color'),
                 TextColumn::make('start_at'),
@@ -55,21 +60,15 @@ class EventResource extends Resource
             ->filters([
                 //
             ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+            ->actions([Tables\Actions\EditAction::make()])
+            ->bulkActions([Tables\Actions\BulkActionGroup::make([Tables\Actions\DeleteBulkAction::make()])]);
     }
 
     public static function getRelations(): array
     {
         return [
-            //
-        ];
+                //
+            ];
     }
 
     public static function getPages(): array
